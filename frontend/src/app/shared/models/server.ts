83c40f794976da0afa414027d301bbe7f2b5424a
Matthew Potts <dev.matthewpotts@gmail.com>
2021-02-03 19:40:08 -0700

    Added NewServer interface that only includes the fields needed to send a request to create a server.

--- frontend/src/app/shared/models/server.ts
+++ frontend/src/app/shared/models/server.ts
@@ -1,28 +1,58 @@
 import { Channel } from "./channel";
+import { User } from "./user";
 
 export interface Server {
     id: number
     name: string
-    description: string
     image: string
+    description: string 
+    role: Role
     roles: Role[]
     channels: Channel[]
 }
+
+export interface NewServer {
+    name:string
+    description:string
+}
+    // updateRole(r: Role) {
+    //     let role = this.roles.find(function (role) {
+    //         return role.id == r.id;
+    //     });
+    //     if (role) {
+    //         role = r;
+    //     } else {
+    //         this.roles.push(r);
+    //     }
+    // }
 
 export interface Role {
     id?: number
     name: string
     serverPermissions: number
+    ChannelPermissions: ChannelPermissions[]
+}
+
+export interface ChannelPermissions {
+	channelID: number   
+	value:     number 
 }
 
 export interface Invite {
     code: string
 }
 
-// ServerRequest .
 export interface ServerRequest {
     serverID: number
-    Channel?: Channel
-    Role?: Role
-    Roles?: Role[]
+    channel?: Channel
+    role?: Role
+    roles?: Role[]
+}
+
+export interface Update {
+    servers?: Server[]
+    serverID: number
+    channelID: number;
+    channel?: Channel
+    role?: Role
 }
