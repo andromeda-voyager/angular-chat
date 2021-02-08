69edc9b4f03a2cbd227ade51c28f6b14268a3b4c
Matthew Potts <dev.matthewpotts@gmail.com>
2021-02-08 10:00:17 -0700

    Added Override struct to hold channel permission values for roles that are overriden for a channel. Added Ovverrides field to Channel struct.

--- backend/server/channel.go
+++ backend/server/channel.go
@@ -7,44 +7,68 @@ import (
 
 // Channel .
 type Channel struct {
-	ID       int        `json:"id"`
-	Name     string     `json:"name"`
-	Messages []*Message `json:"messages"`
+	ID        int        `json:"id"`
+	ServerID  int        `json:"serverID"`
+	Name      string     `json:"name"`
+	Overrides []Override `json:"overrides"`
+}
+
+// Override .
+type Override struct {
+	RoleID      int   `json:"roleID"`
+	Permissions uint8 `json:"permissions"`
 }
 
 // NewChannel .
-func NewChannel(channel Channel, rolesWithAccess []Role, serverID int) *Channel {
+func NewChannel(c *Channel) {
 	var args []interface{}
-	args = append(args, serverID, channel.Name)
+	args = append(args, c.ServerID, c.Name)
 	channelID, err := database.Exec("INSERT INTO Channel (server_id, name) Values (?, ?);", args)
 	if err != nil {
 		panic(err.Error())
 	}
-	var c = &Channel{ID: channelID, Name: channel.Name, Messages: nil}
-	// ok := validateRoles(rolesWithAccess, serverID)
-	// if ok {
-	c.AddPermissions(rolesWithAccess)
-	//	}
-	return c
+	c.ID = channelID
+	c.AddOverrides()
+
 }
 
-// getChannels .
-func (c Channel) getPosts() {
+// LoadChannelOverrides .
+func (c Channel) LoadOverrides() {
 	var args []interface{}
 	args = append(args, c.ID)
 	rows, err := database.Query(
-		`SELECT text, media, time_sent, account_id
-		FROM Message 
-		where Message.channel_id=?;`, args)
+		`SELECT role_id, permissions
+		FROM ChannelPermissions
+		where channel_id=?;`, args)
 	if err != nil {
 		panic(err.Error())
 	}
 	defer rows.Close()
+	c.Overrides = []Override{}
 	for rows.Next() {
-		var m Message
-		rows.Scan(&m.Text, &m.Media, &m.TimePosted, &m.AccountID)
-		c.Messages = append(c.Messages, &m)
+		var o Override
+		rows.Scan(&o.RoleID, &o.Permissions)
+		c.Overrides = append(c.Overrides, o)
 	}
+}
+
+// getChannels .
+func (c Channel) getPosts() {
+	// var args []interface{}
+	// args = append(args, c.ID)
+	// rows, err := database.Query(
+	// 	`SELECT text, media, time_sent, account_id
+	// 	FROM Message
+	// 	where Message.channel_id=?;`, args)
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+	// defer rows.Close()
+	// for rows.Next() {
+	// 	var m Message
+	// 	rows.Scan(&m.Text, &m.Media, &m.TimePosted, &m.AccountID)
+	// 	c.Messages = append(c.Messages, &m)
+	// }
 }
 
 // func validateRoles(rolesWithAccess []Role, serverID int) bool {
@@ -62,10 +86,10 @@ func (c Channel) getPosts() {
 // }
 
 // AddPermissions .
-func (c *Channel) AddPermissions(roles []Role) {
-	for _, r := range roles {
+func (c *Channel) AddOverrides() {
+	for _, r := range c.Overrides {
 		var args []interface{}
-		args = append(args, r.ID, c.ID, permissions.None)
+		args = append(args, r.RoleID, c.ID, permissions.None)
 		_, err := database.Exec("INSERT INTO ChannelPermissions (role_id, channel_id, permissions) Values (?, ?, ?);", args)
 		if err != nil {
 			panic(err.Error())
