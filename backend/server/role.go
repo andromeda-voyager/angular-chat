b957d1da1636adee2858f2729e37d636ace0d15a
Matthew Potts <dev.matthewpotts@gmail.com>
2021-02-08 10:01:57 -0700

    Removed ChannelPermisions and added a similar struct called Override to Channel struct. ServerPermissions field renamed to Permissions.

--- backend/server/role.go
+++ backend/server/role.go
@@ -1,38 +1,9 @@
 package server
 
-import "nebula/database"
-
 // Role .
 type Role struct {
-	ID                 int                  `json:"id"`
-	Ranking            int                  `json:"ranking"`
-	Name               string               `json:"name"`
-	ServerPermissions  uint8                `json:"serverPermissions"`
-	ChannelPermissions []ChannelPermissions `json:"channelPermissions"`
-}
-
-// ChannelPermissions .
-type ChannelPermissions struct {
-	ChannelID int   `json:"channelID"`
-	Value     uint8 `json:"value"`
-}
-
-// LoadChannelPermissions .
-func (r Role) LoadChannelPermissions() {
-	var args []interface{}
-	args = append(args, r.ID)
-	rows, err := database.Query(
-		`SELECT channel_id, permissions
-		FROM ChannelPermissions
-		where role_id=?;`, args)
-	if err != nil {
-		panic(err.Error())
-	}
-	defer rows.Close()
-	var permissions []ChannelPermissions
-	for rows.Next() {
-		var p ChannelPermissions
-		rows.Scan(&p.ChannelID, &p.Value)
-		permissions = append(permissions, p)
-	}
+	ID          int    `json:"id"`
+	Ranking     int    `json:"ranking"`
+	Name        string `json:"name"`
+	Permissions uint8  `json:"permissions"`
 }
