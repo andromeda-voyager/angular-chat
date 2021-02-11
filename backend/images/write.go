bacb5bef5cceff0473d53f42a0190f80d6ff73d6
Matthew Potts <dev.matthewpotts@gmail.com>
2021-02-11 11:38:09 -0700

    Fixed bug where baseUrl was not added to default images.

--- backend/images/write.go
+++ backend/images/write.go
@@ -19,18 +19,18 @@ func Save(r *http.Request, defaultUrl string) string {
 	imageFile, _, err := r.FormFile("image")
 	if err != nil {
 		fmt.Println("No image provided. Using default image.")
-		return defaultUrl
-	}
-	fileName = random.NewString(10) + ".jpg"
-	out, err := os.Create(path.Join(avatarFolder, fileName)) //header.Filename
-	if err != nil {
-		fmt.Println("Dailed to save image. Using default.")
-		return defaultUrl
+		fileName = defaultUrl
 	} else {
-		defer out.Close()
-		defer imageFile.Close()
-		io.Copy(out, imageFile)
+		fileName = random.NewString(10) + ".jpg"
+		out, err := os.Create(path.Join(avatarFolder, fileName)) //header.Filename
+		if err != nil {
+			fmt.Println("Dailed to save image. Using default.")
+			return defaultUrl
+		} else {
+			defer out.Close()
+			defer imageFile.Close()
+			io.Copy(out, imageFile)
+		}
 	}
-
 	return avatarBaseURL + fileName
 }
