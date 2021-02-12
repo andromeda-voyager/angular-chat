444c7719c4d9861c78ebd2f83b89271ec7f6cef0
Matthew Potts <dev.matthewpotts@gmail.com>
2021-02-12 14:51:27 -0700

    Added more documentation.

--- backend/router/context.go
+++ backend/router/context.go
@@ -2,10 +2,13 @@ package router
 
 import "strconv"
 
+// Context is used to store values set by the middleware and path parameters
 type Context struct {
 	Keys map[string]interface{}
 }
 
+// addPathParam sets a context value for a path parameter used in a route. If
+// the path paramter is supposed to be an int, it is converted to an int.
 func (c *Context) addPathParam(r *route, value string) bool {
 	if r.isIntParam {
 		intValue, err := strconv.Atoi(value)
--- backend/router/router.go
+++ backend/router/router.go
@@ -1,55 +1,48 @@
 package router
 
 import (
-	"fmt"
 	"net/http"
 )
 
-type account struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Avatar   string `json:"avatar"`
-	Email    string `json:"email"`
-	Code     string `json:"code"`
-}
-
+// root route for all nested routes
 var routes *route
 
 func init() {
 	routes = &route{name: "/", nestedRoutes: make(map[string]*route)}
 }
 
+// routeCallbackFunc defines the method definition for a route callback function
 type routeCallbackFunc func(w http.ResponseWriter, r *http.Request, c *Context)
 
 func setHeaders(w *http.ResponseWriter, r *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
-	(*w).Header().Set("Access-Control-Allow-Headers", "withCredentials, Origin, Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-	(*w).Header().Set("Content-Type", "application/json")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	// (*w).Header().Set("Content-Type", "application/json")
+	// (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
 }
 
-// Post registers a callback function for the provided path
+// Post registers the provided callback and group that calls it with a POST route
 func (g *Group) Post(path string, callback routeCallbackFunc) {
-	routes.Build(splitPath(path, "POST"), callback, g)
+	routes.build(splitPath(path, "POST"), callback, g)
 }
 
-// Delete registers a callback function for the provided path
+// Delete registers the provided callback and group that calls it with a DELETE route
 func (g *Group) Delete(path string, callback routeCallbackFunc) {
-	routes.Build(splitPath(path, "DELETE"), callback, g)
+	routes.build(splitPath(path, "DELETE"), callback, g)
 }
 
-// Get registers a callback function for the provided path
+// Get registers the provided callback and group that calls it with a GET route
 func (g *Group) Get(path string, callback routeCallbackFunc) {
-	routes.Build(splitPath(path, "GET"), callback, g)
+	routes.build(splitPath(path, "GET"), callback, g)
 }
 
-// Handler is the router handler function to route paths to functions registered with the router
+// Handler is called by http.HandleFunc. The match function is called on the root route object
+// to find the route assoicated with the req url path.
 func Handler(w http.ResponseWriter, req *http.Request) {
 	setHeaders(&w, req)
 	if req.Method != "OPTIONS" {
-		fmt.Println(req.URL.Path)
 		c := &Context{Keys: make(map[string]interface{})}
-		routes.Match(splitPath(req.URL.Path, req.Method), w, req, c)
+		routes.match(splitPath(req.URL.Path, req.Method), w, req, c)
 	}
 }
