bcd93d261edf9889c1e5948b0a99b5096f164fa7
Matthew Potts <72045364+andromeda-voyager@users.noreply.github.com>
2021-05-18 10:15:46 -0600

    Create credentials.template.go

---
+++ backend/config/credentials.template.go
@@ -0,0 +1,12 @@
+package config
+
+const (
+	// DatabaseUser is the name of the account used for database opeations
+	DatabaseUser = ""
+	// DatabasePassword for Username
+	DatabasePassword = ""
+	// EmailAccount .
+	EmailAccount = ""
+	// EmailPassword .
+	EmailPassword = ""
+)
